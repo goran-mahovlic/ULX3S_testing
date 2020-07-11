995ef8d629b69e1c55f5b2a0560c1a6814a40ecf
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-11 09:06:24 +0200

    use tty subsystem and emit properties to get serial

--- testing.pl
+++ testing.pl
@@ -6,6 +6,8 @@ use English;
 
 use Data::Dump qw(dump);
 
+my $debug = $ENV{DEBUG} || 0; # use DEBUG=1 to enable debug
+
 die "need to run $0 as root\n" unless $UID == 0;
 
 my $power_hubs;
@@ -27,16 +29,29 @@ close($uhubctl);
 
 warn "# power_hubs = ",dump($power_hubs);
 
-open(my $udev, '-|', 'udevadm monitor --kernel --subsystem-match usb-serial');
+open(my $udev, '-|', 'udevadm monitor --udev --subsystem-match tty --property');
 while(<$udev>) {
 	chomp;
-	if (m/\S+ bind\s+(\S+) \(usb-serial\)/ ) {
+	if (m/\S+ add\s+(\S+) \(tty\)/ ) {
 		my $path = $1;
 		my @p = split(/\//, $path);
 		my $dev  = $p[-1];
-		my $port = $p[-3];
-		my $hub  = $p[-4];
+		my $port = $p[-5];
+		my $hub  = $p[-6];
 		$port =~ s/^$hub\.//; # strip hub from port identifier
+
+		# read property
+		my $prop;
+		while(<$udev>) {
+			chomp;
+			last if $_ eq '';
+			my ( $name, $val ) = split(/=/,$_,2);
+			$prop->{$name} = $val;
+		}
+
+		print "# prop = ",dump($prop);
+
+
 		if ( exists $power_hubs->{$hub}->{$port} ) {
 			print "FOUND $dev on hub $hub port $port from $path\n";
 			print "FIXME to power-cycle use: uhubctl -l $hub -p $port -a 2\n";
@@ -44,7 +59,7 @@ while(<$udev>) {
 			print "ERROR: $dev on hub $hub port $port NOT POWER CAPABLE!\n";
 		}
 	} else {
-		warn "IGNORE udev: $_\n";
+		warn "IGNORE udev: $_\n" if $debug;
 	}
 
 }
