de4c18967928f0fe5868d5bcad004463d35f8b6b
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-12 14:45:33 +0200

    get fpga size using openFPGALoader

--- testing.pl
+++ testing.pl
@@ -27,7 +27,12 @@ while(<$uhubctl>) {
 }
 close($uhubctl);
 
-warn "# power_hubs = ",dump($power_hubs);
+warn "# power_hubs = ",dump($power_hubs) if $debug;
+
+print "Plug in some FPGA boards or power cycle ports using uhubctl!\n";
+
+my $seen_serial; # state machine for serial
+my $data; # collected data about this serial
 
 open(my $udev, '-|', 'udevadm monitor --udev --subsystem-match tty --property');
 while(<$udev>) {
@@ -49,12 +54,30 @@ while(<$udev>) {
 			$prop->{$name} = $val;
 		}
 
-		print "# prop = ",dump($prop);
-
+		my $serial = $prop->{ID_SERIAL_SHORT} || die "can't find ID_SERIAL_SHORT in prop = ",dump($prop);
 
 		if ( exists $power_hubs->{$hub}->{$port} ) {
-			print "FOUND $dev on hub $hub port $port from $path\n";
-			print "FIXME to power-cycle use: uhubctl -l $hub -p $port -a 2\n";
+		        if ( ! exists $seen_serial->{ $serial } ) {
+				$seen_serial->{ $serial } = 1;
+
+				print "FOUND $serial $dev on hub $hub port $port from $path\n";
+				print "FIXME to power-cycle use: uhubctl -l $hub -p $port -a 2\n";
+
+				open(my $model, '-|', "openFPGALoader --board=ulx3s --detect --device=$prop->{DEVNAME}");
+				while(<$model>) {
+					chomp;
+					if ( m/model\s+LFE5U-(\d+)/ ) {
+						$data->{$serial}->{fpga_size} = $1;
+						print "FOUND $serial is $1 size\n";
+					}
+					warn "# $_\n"; # if $debug;
+				}
+				close($model);
+
+			} else {
+				warn "UNHANDLED seen_serial state ", $seen_serial->{ $serial }, " prop = ",dump($prop), "\ndata $serial = ",dump( $data->{ $serial } );
+			}
+
 		} else {
 			print "ERROR: $dev on hub $hub port $port NOT POWER CAPABLE!\n";
 		}
