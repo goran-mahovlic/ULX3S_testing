d872a097309ba74255c469093e081739effa5b98
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-12 13:32:40 +0200

    install ulx3s-toolchain for support tools

--- debian-install.sh
+++ debian-install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -xe
 
 sudo apt-get install uhubctl libdata-dump-perl
+
+# use for fujprog, ftx_prog and friends
+dpkg -l ulx3s-toolchain || ( wget https://github.com/alpin3/ulx3s/releases/download/v2020.07.12/ulx3s-toolchain_2020.07.12-2_amd64.deb && dpkg -i ulx3s-toolchain_*.deb )
