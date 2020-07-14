3bd0b5411c9902ca98c49a488a720d88fbd9f090
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-14 17:38:09 +0200

    use ftx_prog from ulx3-bin

--- backup/ftx-backup.sh
+++ backup/ftx-backup.sh
@@ -2,5 +2,5 @@
 
 test -z "$1" && echo "usage: $0 serial" && exit 1
 
-ftx_prog --old-serial-number $1 --save $1 | tee $1.txt
+../ulx3s-bin/usb-jtag/linux-amd64/ftx_prog --old-serial-number $1 --save $1 | tee $1.txt
 
