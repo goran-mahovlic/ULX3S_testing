bf445014e526e69bc59008b5d02308f09a9c7e95
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-15 11:34:25 +0200

    download micropython to blob/esp32/

--- blob-download.sh
+++ blob-download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
-test -e blob || mkdir blob
-cd blob
+test -e blob/esp32 || mkdir -p blob/esp32
+cd blob/esp32
 
 # latest stable idf3 version from https://micropython.org/download/esp32/
 wget --no-check-certificate https://micropython.org/resources/firmware/esp32-idf3-20191220-v1.12.bin
