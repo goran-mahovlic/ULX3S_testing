22d5c9254ad8ac58be6ec39b0941284abd00b39a
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-23 10:44:39 +0200

    add explicit sync when writing to sdcard

--- make-sdcard.sh
+++ make-sdcard.sh
@@ -2,4 +2,4 @@
 
 test -z "$1" && echo "Usage: $0 /dev/mmcblk0" && exit 1
 
-gzip -cd blob/ulx3s-saxonsoc/v2020.04.20/saxonsoc-sdimage.raw.gz | dd status=progress bs=1M of=$1
+gzip -cd blob/ulx3s-saxonsoc/v2020.04.20/saxonsoc-sdimage.raw.gz | dd iflag=fullblock oflag=direct conv=fsync status=progress bs=1M of=$1
