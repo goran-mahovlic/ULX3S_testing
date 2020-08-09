e2f590b5ec3b77beadc50cc55a58a37bfa1d31fe
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-08-09 14:08:30 +0200

    we need both python(2) and python3

--- debian-install.sh
+++ debian-install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
-apt-get install uhubctl libdata-dump-perl libfile-slurp-perl libdevice-serialport-perl
+apt-get install uhubctl libdata-dump-perl libfile-slurp-perl libdevice-serialport-perl python python-serial python3 python3-serial
 
 # use for fujprog, ftx_prog and friends
 dpkg -l ulx3s-toolchain || ( wget https://github.com/alpin3/ulx3s/releases/download/v2020.07.12/ulx3s-toolchain_2020.07.12-2_amd64.deb && dpkg -i ulx3s-toolchain_*.deb )
